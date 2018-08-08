5ddcf06489fc150c22a8e39b170405fc04ad419f
Erland <33587369+blackadress@users.noreply.github.com>
2018-08-08 12:18:33 -0500

    Modelos Pago y Requisitos
    Modelos Pago y Requisitos agregados

--- pago/models.py
+++ pago/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from usuario.models import Usuario
 # Create your models here.
+
+class Pago(models.Model):
+    usuario_asignado = models.ForeignKey(Usuario, related_name = 'usuarioAsignado',on_delete=models.PROTECT)
+    usuario_paga = models.ForeignKey(Usuario, related_name = 'usuarioPaga',on_delete=models.PROTECT)
+    fecha = models.DateTimeField('fecha de pago')
+    estado = models.BooleanField(default=False)
+
+class Requisitos(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField('fecha de creación')
+    fecha_ultima_modificacion = models.DateTimeField('última modificación')
+    estado = models.BooleanField(default=False)
