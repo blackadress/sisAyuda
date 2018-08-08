6623e49de3178911d1be9de3395368497706dd2f
Erland <33587369+blackadress@users.noreply.github.com>
2018-08-08 11:59:00 -0500

    Subtarea Modelo Solicitud
    Modelo solicitud y modelo usuario creado

--- solicitud/models.py
+++ solicitud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from usuario.models import Usuario
 # Create your models here.
+
+class Solicitud(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha = models.DateTimeField('fecha de solicitud')
+    monto = models.DecimalField(max_digits =10 ,decimal_places=2)
+    estado = models.BooleanField(default=False)
--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
