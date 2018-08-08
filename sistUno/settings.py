b8d8a22077632add86af44de76e5e8086cb6ba03
Erland <33587369+blackadress@users.noreply.github.com>
2018-08-08 10:42:48 -0500

    primer commit
    Conexion con BD
    Estructura básica creada

--- sistUno/settings.py
+++ sistUno/settings.py
@@ -37,9 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Propias
-    'pagos.apps.PagosConfig',
 ]
 
 MIDDLEWARE = [
